6c059d238b75d6770bceb048cc2f470272d62f19
TrishonParrilla <trishonp1@gmail.com>
2026-01-26 20:38:47 -0700

    Simple javascript introductory practice in console

---
+++ scripts/variables.js
@@ -0,0 +1,62 @@
+console.log("Variable");
+
+
+//string
+let firstName = "Samantha";
+//numerical
+let myAge = 99;
+//boolean
+let isProfessor = true;
+let isStudent = false;
+
+//display the values
+console.log(isProfessor)
+console.log(isStudent)
+
+/* make a variable for your last name
+make a variable for height
+show each variable seperately in the console. */
+
+
+let lastName = "Parrilla"
+let height = 69
+
+console.log(lastName);
+console.log(height);
+
+//arithmetic operations
+
+let num1 = 10;
+let num2 = 20;
+
+let sum = num1 + num2;
+let subs = num1 - num2;
+let mult = num1 * num2;
+let div = num1/num2;
+
+console.log(sum);
+console.log(`${num1} + ${num2} = ${sum} `);
+console.log(`${num1} - ${num2} = ${subs}`);
+console.log(`${num1} * ${num2} = ${mult}`);
+console.log(`${num1} / ${num2} = ${div}`);
+
+let radius = 3;
+const PI = 3.14;
+
+let area = PI * radius * radius
+
+console.log(`The area of a circle with ${radius} is ${area}`);
+
+// difference between variable and constant
+let aVariable; // create a variable
+aVariable = 20; //assign a value
+aVariable = 5; 
+// create and assign a value 
+let anotherVariable = 30;
+
+console.log(aVariable);
+
+const weekDays = 7;
+
+//weekdays = 9;
+console.log(weekDays);
